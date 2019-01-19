392076cbd86426c514b60df466953d79f7e3059c
tmdava <41838954+tmdava@users.noreply.github.com>
2019-01-19 11:38:57 -0600

    add subtruct method

--- src/calc.java
+++ src/calc.java
@@ -5,5 +5,7 @@ public class calc {
         System.out.println("hello dovlet");
         System.out.println("Hello world");
     }
-
+    public static double subtract(double num1, double num2){
+        return num1-num2;
+    }
 }
